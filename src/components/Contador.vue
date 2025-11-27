fb0d6ad05c95676cca03c6982d4114699486f3a9
Mateojami10 <majamic@uce.edu.ec>
2025-11-26 19:44:50 -0500

    Taller 14: Directivas en Vue

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,15 +1,81 @@
 <template>
     <div>
-        <h1>Universidad Central del Ecuador</h1>
-        <h2>Carrera de Computación</h2>
+        <h1>{{tit}}</h1>
+        <p>{{numero}} <sup>2</sup>= {{calculaCuadrado}}</p>
+        <p>{{numero}} <sup>2</sup>= {{calculaCuadrado}}</p>
+        <p>{{numero}} <sup>2</sup>= {{calculaCuadrado}}</p>
+        <p>{{numero}} <sup>2</sup>= {{calculaCuadrado}}</p>
+        <p>{{numero}} <sup>2</sup>= {{calcularCubo}}</p>
+        <p>{{numero}} <sup>2</sup>= {{obtenerCubo()}}</p>
+
+        <div>
+            <button v-on:click="incrementar">+</button>
+            <button @:click="decrementar">-</button>
+            <button v-show="mostrar">prueba</button>
+        </div>
+
+      
+        
     </div>
   
 </template>
 
-<script> 
-export default {
+<script>
+import { computed } from 'vue';
 
-}
+ 
+export default {
+    data() {
+        return {
+            numero: this.inicio,
+            titulo: 'Contador',
+            
+        };
+    },
+
+    methods: {
+        obtenerCuadrado() {
+            console.log("Entro a obtenerCuadrado");
+            return this.numero * this.numero;
+        },
+        obtenerCubo() {
+            console.log("Entro a obtenerCubo");
+            return this.numero * this.numero * this.numero;
+        },
+        incrementar() {
+            console.log(this.tit);
+            this.numero++;
+        },
+        decrementar() {
+            this.numero--;
+        },
+    },
+    computed: {
+        calculaCuadrado() {
+            console.log("Entro a propiedad computada");
+            return this.numero * this.numero;
+        },
+        calcularCubo() {
+            console.log("Entro a propiedad computada del cubo");
+            return this.numero * this.numero * this.numero;
+        },
+    },
+   /* props: ['tit', 'inicio', 'mostrar'], declaracion basica de props*/
+   props: {
+        tit: String,
+        inicio: {
+            type: Number,
+            default: 99,
+            validator(value) {
+                return value > 10; /*mientras el valor de inicio sea mayor que 10 funciona */
+            }
+        },
+        mostrar: Boolean
+    }
+    /*String, Number, Boolean, Array, Object, Date, Function, BigInt (todos los tipos de props)*/
+    
+};
+
 </script>
 <style> 
 
