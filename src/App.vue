114eee3b6f7ceb8313512ba5d29c9f4ef4d70bc4
Mateojami10 <majamic@uce.edu.ec>
2025-11-27 19:36:45 -0500

    Taller 15: Directivas en Vue parte 2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <ContadorView />
+    <!----<ContadorView />--->
+    <DirectivasView />
   </div>
 </template>
 
 <script>
 import ContadorView from '../src/views/ContadorView.vue';
+import DirectivasView from '../src/views/DirectivasView.vue';  
 export default {
+  name: 'App',
   components: {
     ContadorView,
+    DirectivasView,
   },
 };
 </script>
