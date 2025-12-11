a393f98b948c47da43161a50f9fc352f303318e3
Mateojami10 <majamic@uce.edu.ec>
2025-12-10 19:45:54 -0500

    Taller 17: Clientes Api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <!----<ContadorView />--->
-    <DirectivasView />
+    <!---<ContadorView />-->
+    <!--<DirectivasView />  -->
+    <PreguntaView />
   </div>
 </template>
 
 <script>
 import ContadorView from '../src/views/ContadorView.vue';
 import DirectivasView from '../src/views/DirectivasView.vue';  
+import PreguntaView from '../src/views/PreguntaView.vue';
 export default {
   name: 'App',
   components: {
     ContadorView,
     DirectivasView,
+    PreguntaView,
   },
 };
 </script>
